36833bf26696dcac5b9f36453beb75b9e18cb896
(Benjamin Olivera) <unknowsoldierbj@gmail.com>
2022-01-17 16:36:28 -0500

    stupid coaching

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "ask", to: "questions#ask"
 end
+# questions es un controlador y ask es la accion que debo buscar
